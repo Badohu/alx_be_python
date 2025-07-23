108f9165cc231c6376fdb8ea95f3dc279c5444d6
Badohu <thomasbadohu40@gmail.com>
2025-07-23 09:53:03 +0000

    alx_be_python

---
+++ weather_advice.py
@@ -0,0 +1,9 @@
+weather=input("whats the weather like today?(sunny/raimy/cold):").lower()
+if weather=="sunny":
+ print("Its sunny! You might want to wear light clothing like  a t-shirt and shorts.Dont forget sunscreen and sunglasses!")
+elif weather =="rainy":
+ print ("its rainy! grab an umbrella and a raincoat.Waterproof shoes would be a great idea.")
+elif weather =="cold":
+ print ("Its cold!DRess warmly with a heavy coat,a hat,gloves,and maybe a scarf.")
+else :
+ print ("Im not sue about that weather conditions.Please enter 'sunny','raint','cold'")
